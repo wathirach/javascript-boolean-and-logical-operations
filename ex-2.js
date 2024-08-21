889f60de83a9d268516e6fc87d30dc7c716c29ae
Athirach <wathirach@gmail.com>
2024-08-21 22:16:18 +0700

    Bp5-wan: boolean and logical operations

--- ex-2.js
+++ ex-2.js
@@ -7,6 +7,12 @@ let hasBoughtProductFromITCategory;
 let hasAttendedDiscountEvent;
 let isPlatinum;
 
-let hasPromotion;
+lastMonthPaidMoreThan4000 = true;
+isWeekday = true;
+hasBoughtProductFromITCategory = true;
+hasAttendedDiscountEvent = false;
+isPlatinum = false;
 
+let hasPromotion;
+hasPromotion = (lastMonthPaidMoreThan4000 && isWeekday && hasBoughtProductFromITCategory &&!hasAttendedDiscountEvent) || isPlatinum;
 console.log(hasPromotion);
